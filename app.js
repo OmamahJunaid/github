d03a9c9fb44a000f79f582600245d352296c6d0d
OmamahJunaid <142996131+OmamahJunaid@users.noreply.github.com>
2023-09-18 21:39:11 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,76 @@
-// Chapter 01
-// Question 1
-alert("Welcome");
-// 
-alert("Error! Please enter a valid password")
+// CHAPTER 01
+// Q01
+alert("First name");
+alert("Last name");
+alert("Email");
+alert("Phone number");
+alert("Password");
+// Q02
+
+alert("You're learning JavaScript!");
+// Q03
+alert("welcome to learning javaScript");
+//CHAPTER 02
+// Q01
+var myName ="Omamah Junaid";
+// Q02
+var num1;
+num1="One";
+// Q03
+var teamName ="RED";
+alert(teamName);
+// Q04
+var bestMan = "Charlie";
+bestMan =" Mark";
+// CHAPTER 04
+// Q01
+var caseQty;
+// Q02
+caseQty= 144 ;
+// Q03
+var num = "9";
+// Q04
+var sum;
+sum=8+2
+// Q05
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+// Q06
+var number = 2
+number = 2+4
+// CHAPTE04
+// Q01
+var productcost = 3.45;
+// Q02
+var nameOfBand;
+// Q03
+var number1 = 10;
+// Q04
+var omamahJunaid;
+// Q05
+// LEGAL VARIABLES
+document.write("Legal Variables <br> 1)firstName 2)number12 3)my12Num 4)team_mates")
+// ILLEGAL VARIABLES
+document.write(" <br>Illegal Variables <br> 1)first Name 2) 1st 3)*Num 4) 12teamName")
+
+// CHAPTER 05
+// Q01
+Modulus;
+// Q02
+var num = 20 % 6;
+num = 2;
+//  Q03
+ var largeNum = 2000000;
+//  Q04
+var no1 = 2;
+var n02 = 4;
+var result = no1-n02;
+console.log(result);
+// Q05
+var rem = 2;
+console.log(rem);
+// Q06
+alert(4*8);
+
