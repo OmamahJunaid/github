89955407f2b4356dfd368f48374d0db3b3af3593
OmamahJunaid <142996131+OmamahJunaid@users.noreply.github.com>
2023-11-26 14:18:47 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,76 +1,97 @@
-// CHAPTER 01
-// Q01
-alert("First name");
-alert("Last name");
-alert("Email");
-alert("Phone number");
-alert("Password");
-// Q02
+var firebaseConfig = {
+    apiKey: "AIzaSyD2IfXoIPQwVSvhzurWCPochFRzBqbxUFg",
+  authDomain: "shoogaree-3e072.firebaseapp.com",
+  databaseURL: "https://shoogaree-3e072-default-rtdb.firebaseio.com",
+  projectId: "shoogaree-3e072",
+  storageBucket: "shoogaree-3e072.appspot.com",
+  messagingSenderId: "898609609770",
+  appId: "1:898609609770:web:428d23f62c80ce9601d753"
+  };
+      
+      // Initialize Firebase
+var app = firebase.initializeApp(firebaseConfig);
+console.log(app.database);
 
-alert("You're learning JavaScript!");
-// Q03
-alert("welcome to learning javaScript");
-//CHAPTER 02
-// Q01
-var myName ="Omamah Junaid";
-// Q02
-var num1;
-num1="One";
-// Q03
-var teamName ="RED";
-alert(teamName);
-// Q04
-var bestMan = "Charlie";
-bestMan =" Mark";
-// CHAPTER 04
-// Q01
-var caseQty;
-// Q02
-caseQty= 144 ;
-// Q03
-var num = "9";
-// Q04
-var sum;
-sum=8+2
-// Q05
-var merchTotal = 100;
-var shippingCharge = 10;
-var orderTotal = merchTotal + shippingCharge;
-console.log(orderTotal);
-// Q06
-var number = 2
-number = 2+4
-// CHAPTE04
-// Q01
-var productcost = 3.45;
-// Q02
-var nameOfBand;
-// Q03
-var number1 = 10;
-// Q04
-var omamahJunaid;
-// Q05
-// LEGAL VARIABLES
-document.write("Legal Variables <br> 1)firstName 2)number12 3)my12Num 4)team_mates")
-// ILLEGAL VARIABLES
-document.write(" <br>Illegal Variables <br> 1)first Name 2) 1st 3)*Num 4) 12teamName")
+function thanks(){
+    var Ordered = document.getElementById("Section5")
+    Ordered.innerHTML=`<div class="container text-center">
+    <h1 style="color:#FA8DA2; font-family: 'Assistant', sans-serif;
+              font-family: 'Borel', cursive;
+              font-family: 'Noto Nastaliq Urdu', serif;
+              font-family: 'Poiret One', cursive;
+              font-family: 'Roboto Slab', serif; font-size: 80px;"> <br>
+                  Thanks For Ordering</h1> 
+                  <p>Shoogaree Team Will Contact You Soon!!</p>
+  
+  </div>
+  `
+}
 
-// CHAPTER 05
-// Q01
-Modulus;
-// Q02
-var num = 20 % 6;
-num = 2;
-//  Q03
- var largeNum = 2000000;
-//  Q04
-var no1 = 2;
-var n02 = 4;
-var result = no1-n02;
-console.log(result);
-// Q05
-var rem = 2;
-console.log(rem);
-// Q06
-alert(4*8);
 
+
+
+function order(){
+    var name = document.getElementById("name")
+var email = document.getElementById("email")
+var contactNO = document.getElementById("contactNO")
+var altNo = document.getElementById("altNo")
+var fp = document.getElementById("f/p")
+var mod = document.getElementById("mod")
+var details = document.getElementById("details")
+var dod = document.getElementById("dod")
+var address = document.getElementById("addressD")
+
+    var obj ={
+        name: name.value,
+        email: email.value,
+        contactNO : contactNO.value,
+        altNo: altNo.value,
+        flavours: fp.value,
+        mod :mod.value,
+        details: details.value,
+        dod :dod.value,
+        address: address.value,
+  
+
+
+
+    }
+   
+    firebase
+    .database()
+    .ref("userDetails")
+    .push(obj);
+    
+console.log(obj)
+thanks()
+
+}
+function getval(){
+    var name = document.getElementById("name")
+    var email = document.getElementById("email")
+    var contactNO = document.getElementById("contactNO")
+    var altNo = document.getElementById("altNo")
+    var fp = document.getElementById("f/p")
+    var mod = document.getElementById("mod")
+    var details = document.getElementById("details")
+    var dod = document.getElementById("dod")
+    var address = document.getElementById("addressD")
+
+
+
+  if (name.value==null || name.value=="") {
+    alert("first fill the entire field..");}
+    else if (contactNO.value==null || contactNO.value=="") {
+        alert("first fill the entire field..");}
+        else if (mod.value==null || mod.value=="") {
+            alert("first fill the entire field..");}
+            else if (dod.value==null || dod.value=="") {
+                alert("first fill the entire field..");} 
+                else if (address.value==null || address.value=="") {
+                    alert("first fill the entire field..");} 
+
+    else {
+        order()
+    }
+  
+}
